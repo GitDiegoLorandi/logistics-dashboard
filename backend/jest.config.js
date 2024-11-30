d1b2716bd4095094cf0ba9de743dde966085d953
Diego Lorandi <diego.lorandi@planaltocapital.com.br>
2024-11-29 21:05:17 -0300

    [ADD] - Auth and routes tests

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  transform: {
-    "^.+\\.[t|j]sx?$": "babel-jest",  // Use babel-jest for all .js and .jsx files
-  },
+  moduleDirectories: ["node_modules", "src"],
+  moduleFileExtensions: ["js", "jsx", "json", "node"],
   testEnvironment: "node",  // Use Node environment for backend tests
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest", // Ensures proper transpilation
+  }
 };
--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,5 +3,7 @@ module.exports = {
       "^.+\\.[t|j]sx?$": "babel-jest",  // Use babel-jest for all .js and .jsx files
     },
     testEnvironment: "node",  // Use Node environment for backend tests
+    moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+      moduleDirectories: ['node_modules', 'src'],
   };
   
