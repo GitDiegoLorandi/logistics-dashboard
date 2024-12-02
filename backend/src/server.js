a59f37a2a35ba4e46657f2cdb9a4da26596803d6
Diego Lorandi <diego.lorandi@planaltocapital.com.br>
2024-12-02 00:40:16 -0300

    [NEW] - Deliverer Routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const deliveryRoutes = require("./routes/deliveryRoutes");
+const delivererRoutes = require("./routes/delivererRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +16,7 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/deliveries", deliveryRoutes);
+app.use("/api/deliverers", delivererRoutes); 
 
 // Database Connection
 mongoose
